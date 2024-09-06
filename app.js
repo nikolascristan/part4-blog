92c2a34353774a97f3f39945a748968450c2ed10
nikolascristan <nikolascristan@gmail.com>
2024-09-06 12:42:24 -0500

    Refactor app

--- app.js
+++ app.js
@@ -2,43 +2,26 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
 mongoose.connect(config.MONGODB_URI)
-
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('error connecting', error.message)
+  })
+  
 app.use(cors())
-app.use(express.json())
+//app.use(express.json())
+app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-
-//Got a working state
+module.exports = app
