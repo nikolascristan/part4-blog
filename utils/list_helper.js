49eb7b838f2c607b64c8bb4ade42c49cbd00c229
nikolascristan <nikolascristan@gmail.com>
2024-09-11 15:38:39 -0500

    Add more helper functions and unit tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
     return(1)
 }
 
+const totalLikes = (blogs) => {
+    const total = (blogs.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue.likes
+    }, 0 ))
+    if (isNaN(total)) {
+        return 0;
+    } else {
+        return total;
+    }
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
